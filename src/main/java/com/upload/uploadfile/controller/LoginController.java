7cba0f2555d24aa90e1115eb354c0a3da042957e
Koray Guney <koray.guney@hotmail.com>
2022-02-22 21:45:27 +0300

    added routing according to role

--- src/main/java/com/upload/uploadfile/controller/LoginController.java
+++ src/main/java/com/upload/uploadfile/controller/LoginController.java
@@ -1,11 +1,21 @@
 package com.upload.uploadfile.controller;
 
+import com.upload.uploadfile.model.User;
+import com.upload.uploadfile.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class LoginController {
+
+    @Autowired
+    UserRepository repository;
 
     @GetMapping("/login")
     public String login(){
